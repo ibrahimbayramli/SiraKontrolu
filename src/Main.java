42a7c2542225b51c435b7b70be65d561061da118
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-06-28 10:22:22 +0300

    2. kodlama ödevi

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //*? Kelime dizisinin girisi.
-        String[] kelimeler = {"ahmet", "ali", "ayşe","mehmet"};
+//        String[] kelimeler = {"ahmet", "ali", "ayşe","mehmet"};
+        String[] kelimeler = {"kabc", "kabcd","kabcde"};
 
         //*? Alfabe siralamasi.
 //        String alfabeSirasi = "defgauğıvijçklümnocöprsyhbştz";
--- src/SiraKontrol.java
+++ src/SiraKontrol.java
@@ -1,6 +1,4 @@
-import javax.xml.crypto.Data;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
+
 
 public class SiraKontrol {
     public static boolean siraKontrol(String[] kelimeler, String alfabeSirasi) {
@@ -8,14 +6,14 @@ public class SiraKontrol {
         boolean result = true;
         for (int i = 0; i < len - 1; i++) {
             int min = Math.min(kelimeler[i].length(), kelimeler[i + 1].length());
-            if(kelimeler[i].length()>kelimeler[i+1].length()){
-                if(kelimeler[i+1].equals(kelimeler[i].substring(0,kelimeler[i+1].length()))){
+            if (kelimeler[i].length() > kelimeler[i + 1].length()) {
+                if (kelimeler[i + 1].equals(kelimeler[i].substring(0, kelimeler[i + 1].length()))) {
                     return false;
                 }
             }
-            if(kelimeler[i+1].length()>kelimeler[i].length()){
-                if(kelimeler[i].equals(kelimeler[i+1].substring(0,kelimeler[i].length()))){
-                    return true;
+            if (kelimeler[i + 1].length() > kelimeler[i].length()) {
+                if (kelimeler[i].equals(kelimeler[i + 1].substring(0, kelimeler[i].length()))) {
+                    continue;
                 }
             }
 
